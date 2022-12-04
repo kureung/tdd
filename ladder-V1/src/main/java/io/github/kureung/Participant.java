0ab5a874904a132a622e1ef0c543b4837765c881
kureungkureung <terry9611@naver.com>
2022-12-04 23:55:38 +0900

    feat: 참여자 이름 글자수 null, empty 일 경우 예외 처리

--- ladder-V1/src/main/java/io/github/kureung/Participant.java
+++ ladder-V1/src/main/java/io/github/kureung/Participant.java
@@ -2,6 +2,10 @@ package io.github.kureung;
 
 public record Participant(String name) {
     public Participant {
+        if (name == null || name.length() == 0) {
+            throw new IllegalArgumentException("이름은 null 값이거나 0자일 수 없다.");
+        }
+
         if (5 < name.length()) {
             throw new IllegalArgumentException("이름은 5글자를 초과할 수 없다.");
         }
--- ladder-V1/src/test/java/io/github/kureung/ParticipantTest.java
+++ ladder-V1/src/test/java/io/github/kureung/ParticipantTest.java
@@ -1,6 +1,8 @@
 package io.github.kureung;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -10,5 +12,13 @@ class ParticipantTest {
         assertThatThrownBy(() -> new Participant("123456"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이름은 5글자를 초과할 수 없다.");
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void 이름은_널값이거나_0자일_수_없다(final String name) {
+        assertThatThrownBy(() -> new Participant(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이름은 null 값이거나 0자일 수 없다.");
     }
 }
