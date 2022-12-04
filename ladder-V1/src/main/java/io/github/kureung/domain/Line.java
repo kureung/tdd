4610c153fe96b02d0e6b5b8a66830c9aec476a5c
kureungkureung <terry9611@naver.com>
2022-12-05 00:15:19 +0900

    feat: 행의 참이 연속될 경우 예외 처리

--- ladder-V1/src/main/java/io/github/kureung/domain/Line.java
+++ ladder-V1/src/main/java/io/github/kureung/domain/Line.java
@@ -4,6 +4,14 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 public record Line(List<Boolean> doRoadsExist) {
+    public Line {
+        for (int i = 0; i < doRoadsExist.size() - 1; i++) {
+            if (doRoadsExist.get(i) && doRoadsExist.get(i + 1)) {
+                throw new IllegalArgumentException("참이 연속해서 올 수 없습니다.");
+            }
+        }
+    }
+
     public Line(final boolean... doRoadsExist) {
         this(convertedCollection(doRoadsExist));
     }
--- ladder-V1/src/test/java/io/github/kureung/domain/LineTest.java
+++ ladder-V1/src/test/java/io/github/kureung/domain/LineTest.java
@@ -5,11 +5,20 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LineTest {
     @Test
     void 부_생성자의_주_생성자_변환_테스트() {
         final List<Boolean> doRoadsExist = List.of(true);
         assertThat(new Line(true)).isEqualTo(new Line(doRoadsExist));
+    }
+
+    @Test
+    void 참이_여러개_일경우_예외가_발생한다() {
+        final List<Boolean> doRoadsExist = List.of(true, true);
+        assertThatThrownBy(() -> new Line(doRoadsExist))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("참이 연속해서 올 수 없습니다.");
     }
 }
