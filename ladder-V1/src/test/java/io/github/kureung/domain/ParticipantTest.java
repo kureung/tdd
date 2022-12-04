4e1656432f3edcb232c40d80d44649d12819a8dd
kureungkureung <terry9611@naver.com>
2022-12-05 00:02:15 +0900

    feat: 참여자 이름 유효한 글자수 테스트

--- ladder-V1/src/test/java/io/github/kureung/domain/ParticipantTest.java
+++ ladder-V1/src/test/java/io/github/kureung/domain/ParticipantTest.java
@@ -3,7 +3,9 @@ package io.github.kureung.domain;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ParticipantTest {
@@ -20,5 +22,15 @@ class ParticipantTest {
         assertThatThrownBy(() -> new Participant(name))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이름은 null 값이거나 0자일 수 없다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "1",
+            "12345"
+    })
+    void 이름은_1자이상_5자이하이어야_한다(final String name) {
+        assertThatCode(() -> new Participant(name))
+                .doesNotThrowAnyException();
     }
 }
