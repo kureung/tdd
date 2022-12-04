c8f19e5ebdac521d8ca37821a77082f665c35203
kureungkureung <terry9611@naver.com>
2022-12-05 01:07:51 +0900

    feat: 행들의 각각 행의 요소 개수가 다르면 예외 처리

--- ladder-V1/src/main/java/io/github/kureung/domain/Lines.java
+++ ladder-V1/src/main/java/io/github/kureung/domain/Lines.java
@@ -4,6 +4,12 @@ import java.util.Arrays;
 import java.util.List;
 
 public record Lines(List<Line> values) {
+    public Lines {
+        if (isNotSameEachLineSize(values)) {
+            throw new IllegalArgumentException("각각의 행들의 요소의 개수가 모두 같아야합니다.");
+        }
+    }
+
     public Lines(final Line... lines) {
         this(convertedCollection(lines));
     }
@@ -11,5 +17,11 @@ public record Lines(List<Line> values) {
     private static List<Line> convertedCollection(Line... lines) {
         return Arrays.stream(lines)
                 .toList();
+    }
+
+    private boolean isNotSameEachLineSize(List<Line> values) {
+        final int firstLineSize = values.get(0).size();
+        return values.stream()
+                .anyMatch(line -> line.isNotSameSize(firstLineSize));
     }
 }
--- ladder-V1/src/test/java/io/github/kureung/domain/LinesTest.java
+++ ladder-V1/src/test/java/io/github/kureung/domain/LinesTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LinesTest {
     @Test
@@ -14,5 +15,14 @@ class LinesTest {
         final List<Line> lines = List.of(firstLine, secondLine);
         assertThat(new Lines(firstLine, secondLine))
                 .isEqualTo(new Lines(lines));
+    }
+
+    @Test
+    void 각각의_행들의_요소의_개수가_모두_같지_않으면_예외가_발생한다() {
+        final Line firstLine = new Line(true);
+        final Line secondLine = new Line(true, false);
+        assertThatThrownBy(() -> new Lines(firstLine, secondLine))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("각각의 행들의 요소의 개수가 모두 같아야합니다.");
     }
 }
