c39b7be3f6d8ad97860397223a071692e43f501d
kureungkureung <terry9611@naver.com>
2022-12-06 23:02:04 +0900

    feat: 방향의 x 좌표 구하기 기능

---
+++ ladder-V2/src/main/java/io/github/kureung/domain/Direction.java
@@ -0,0 +1,20 @@
+package io.github.kureung.domain;
+
+public enum Direction {
+    RIGHT(1, 0),
+    LEFT(-1, 0),
+    BOTTOM(0, -1),
+    ;
+
+    private final int xDistance;
+    private final int yDistance;
+
+    Direction(int xDistance, int yDistance) {
+        this.xDistance = xDistance;
+        this.yDistance = yDistance;
+    }
+
+    public int xCoordinate(int x) {
+        return xDistance + x;
+    }
+}
---
+++ ladder-V2/src/test/java/io/github/kureung/domain/DirectionTest.java
@@ -0,0 +1,26 @@
+package io.github.kureung.domain;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.of;
+
+class DirectionTest {
+    private static Stream<Arguments> x좌표를_알_수_있다() {
+        return Stream.of(
+                of(Direction.RIGHT, 2),
+                of(Direction.LEFT, 0),
+                of(Direction.BOTTOM, 0)
+        );
+    }
+
+    @MethodSource
+    @ParameterizedTest
+    void x좌표를_알_수_있다(Direction direction, int result) {
+        assertThat(direction.xCoordinate(1)).isEqualTo(result);
+    }
+}
