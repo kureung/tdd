fe1d2da3834807001df99ac56fd78f5c2bfe55b0
kureungkureung <terry9611@naver.com>
2022-12-06 23:22:31 +0900

    refactor: 유저 속성에 포지션 추가

--- ladder-V2/src/main/java/io/github/kureung/domain/User.java
+++ ladder-V2/src/main/java/io/github/kureung/domain/User.java
@@ -1,9 +1,15 @@
 package io.github.kureung.domain;
 
-public record User(String name) {
+import io.github.kureung.domain.position.Position;
+
+public record User(String name, Position position) {
     public User {
         if (5 < name.length()) {
             throw new IllegalArgumentException("이름은 5자를 초과할 수 없습니다.");
         }
+    }
+
+    public User(String name, int x) {
+        this(name, Position.firstPoint(x));
     }
 }
--- ladder-V2/src/test/java/io/github/kureung/domain/UserTest.java
+++ ladder-V2/src/test/java/io/github/kureung/domain/UserTest.java
@@ -14,14 +14,14 @@ class UserTest {
             "12345"
     })
     void 유저의_이름은_1자_이상_5자_이하이어야_한다(String name) {
-        assertThatCode(() -> new User(name))
+        assertThatCode(() -> new User(name, 0))
                 .doesNotThrowAnyException();
     }
 
     @Test
     void 유저의_이름은_5자_초과했을_경우_예외가_발생한다() {
         final String name = "123456";
-        assertThatThrownBy(() -> new User(name))
+        assertThatThrownBy(() -> new User(name, 0))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이름은 5자를 초과할 수 없습니다.");
     }
