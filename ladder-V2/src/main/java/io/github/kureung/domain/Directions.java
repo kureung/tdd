368a05a87b8447ae83c2a0861d7642e43b1125bf
kureungkureung <terry9611@naver.com>
2022-12-08 01:20:37 +0900

    feat: 방향들의 유효성 검증

---
+++ ladder-V2/src/main/java/io/github/kureung/domain/Directions.java
@@ -0,0 +1,14 @@
+package io.github.kureung.domain;
+
+import java.util.List;
+
+public record Directions(List<Direction> values) {
+    public Directions {
+        boolean doesNotHaveBottom = values.stream()
+                .allMatch(Direction::isNotBottom);
+
+        if (doesNotHaveBottom) {
+            throw new IllegalArgumentException("무조건 아래로 가는 방향을 가지고 있어야 합니다.");
+        }
+    }
+}
---
+++ ladder-V2/src/test/java/io/github/kureung/domain/DirectionsTest.java
@@ -0,0 +1,19 @@
+package io.github.kureung.domain;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.params.provider.EnumSource.Mode.EXCLUDE;
+
+class DirectionsTest {
+    @ParameterizedTest
+    @EnumSource(mode = EXCLUDE, names = "BOTTOM")
+    void 방향들은_BOTTOM_방향을_가지고_있지_않으면_예외가_발생한다(Direction direction) {
+        assertThatThrownBy(() -> new Directions(List.of(direction)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("무조건 아래로 가는 방향을 가지고 있어야 합니다.");
+    }
+}
