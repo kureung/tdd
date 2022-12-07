855e089f216142709bc81b86745254aad23f7bd2
kureungkureung <terry9611@naver.com>
2022-12-08 01:32:01 +0900

    feat: 방향들에서 아래로가는 방향만 가지고 있으면 참인 기능

--- ladder-V2/src/main/java/io/github/kureung/domain/Directions.java
+++ ladder-V2/src/main/java/io/github/kureung/domain/Directions.java
@@ -11,4 +11,9 @@ public record Directions(List<Direction> values) {
             throw new IllegalArgumentException("무조건 아래로 가는 방향을 가지고 있어야 합니다.");
         }
     }
+
+    public boolean doesHaveOnlyBottom() {
+        return values.stream()
+                .allMatch(Direction::isBottom);
+    }
 }
--- ladder-V2/src/test/java/io/github/kureung/domain/DirectionsTest.java
+++ ladder-V2/src/test/java/io/github/kureung/domain/DirectionsTest.java
@@ -1,11 +1,13 @@
 package io.github.kureung.domain;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.params.provider.EnumSource.Mode.EXCLUDE;
 
 class DirectionsTest {
@@ -15,5 +17,11 @@ class DirectionsTest {
         assertThatThrownBy(() -> new Directions(List.of(direction)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("무조건 아래로 가는 방향을 가지고 있어야 합니다.");
+    }
+
+    @Test
+    void 방향들이_BOTTOM_방향만_가지면_참이다() {
+        final Directions directions = new Directions(List.of(Direction.BOTTOM));
+        assertTrue(directions.doesHaveOnlyBottom());
     }
 }
