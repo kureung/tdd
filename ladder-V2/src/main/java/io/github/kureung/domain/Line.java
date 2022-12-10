70586a56e133117a7614ce96c6e83cb3752d0c41
kureungkureung <terry9611@naver.com>
2022-12-10 14:31:00 +0900

    feat: 행의 유효성 검증

--- ladder-V2/src/main/java/io/github/kureung/domain/Line.java
+++ ladder-V2/src/main/java/io/github/kureung/domain/Line.java
@@ -3,12 +3,15 @@ package io.github.kureung.domain;
 import java.util.Arrays;
 import java.util.List;
 
+import static io.github.kureung.domain.Direction.RIGHT;
+
 public record Line(List<Point> points) {
     public Line {
         for (int index = 0; index < points.size() - 1; index++) {
             final Point currentPoint = points.get(index);
             final Point nextPoint = points.get(index + 1);
             verifySameDirectionsExcludeBottom(currentPoint, nextPoint);
+            verifyRightComeAfterLeft(currentPoint, nextPoint);
         }
     }
 
@@ -23,6 +26,12 @@ public record Line(List<Point> points) {
     private void verifySameDirectionsExcludeBottom(final Point currentPoint, final Point nextPoint) {
         if (currentPoint.doesntHaveOnlyBottom() &&  currentPoint.isSameDirections(nextPoint)) {
             throw new IllegalArgumentException("오른쪽 방향 또는 왼쪽 방향이 연속으로 있을 수 없습니다.");
+        }
+    }
+
+    private void verifyRightComeAfterLeft(final Point currentPoint, final Point nextPoint) {
+        if (currentPoint.isIncludeDirection(RIGHT) &&  nextPoint.isNotIncludeDirection(Direction.LEFT)) {
+            throw new IllegalArgumentException("오른쪽 방향을 가진 지점 다음에는 왼쪽 방향을 가진 지점이 와야합니다.");
         }
     }
 }
--- ladder-V2/src/test/java/io/github/kureung/domain/LineTest.java
+++ ladder-V2/src/test/java/io/github/kureung/domain/LineTest.java
@@ -1,5 +1,6 @@
 package io.github.kureung.domain;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -7,6 +8,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.List;
 import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LineTest {
@@ -25,5 +27,30 @@ class LineTest {
         assertThatThrownBy(() -> new Line(List.of(currentPoint, nextPoint)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("오른쪽 방향 또는 왼쪽 방향이 연속으로 있을 수 없습니다.");
+    }
+
+    @Test
+    void 오른쪽_방향을_가진_지점_다음에는_왼쪽_방향을_가진_지점이_오지_않으면_예외가_발생한다() {
+        final Directions rightAndBottom = new Directions(Direction.RIGHT, Direction.BOTTOM);
+        final Directions onlyBottom = new Directions(Direction.BOTTOM);
+
+        final Point currentPoint = new Point(rightAndBottom, 0);
+        final Point nextPoint = new Point(onlyBottom, 0);
+
+        assertThatThrownBy(() -> new Line(currentPoint, nextPoint))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("오른쪽 방향을 가진 지점 다음에는 왼쪽 방향을 가진 지점이 와야합니다.");
+    }
+
+    @Test
+    void 오른쪽_방향을_가진_지점_다음에는_왼쪽_방향을_가진_지점이_오면_예외가_발생하지_않는다() {
+        final Directions rightAndBottom = new Directions(Direction.RIGHT, Direction.BOTTOM);
+        final Directions leftAndBottom = new Directions(Direction.LEFT, Direction.BOTTOM);
+
+        final Point currentPoint = new Point(rightAndBottom, 0);
+        final Point nextPoint = new Point(leftAndBottom, 0);
+
+        assertThatCode(() -> new Line(currentPoint, nextPoint))
+                .doesNotThrowAnyException();
     }
 }
