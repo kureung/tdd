4f67b3688713e71ebee44548dbaddb5d6d65bcd2
kureungkureung <terry9611@naver.com>
2022-12-10 14:47:20 +0900

    refactor: 타겟 지점 분리

--- ladder-V2/src/test/java/io/github/kureung/domain/LineTest.java
+++ ladder-V2/src/test/java/io/github/kureung/domain/LineTest.java
@@ -10,23 +10,31 @@ import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.params.provider.Arguments.of;
 
 class LineTest {
     private static Stream<Arguments> 오른쪽_방향_또는_왼쪽_방향을_가진_포인트들이_연속으로_등장할_경우_예외가_발생한다() {
         return Stream.of(
-                Arguments.of(new Directions(Direction.LEFT, Direction.BOTTOM)),
-                Arguments.of(new Directions(Direction.RIGHT, Direction.BOTTOM))
+                of(Direction.LEFT, Direction.BOTTOM),
+                of(Direction.RIGHT, Direction.BOTTOM)
         );
     }
 
     @MethodSource
     @ParameterizedTest
-    void 오른쪽_방향_또는_왼쪽_방향을_가진_포인트들이_연속으로_등장할_경우_예외가_발생한다(final Directions directions) {
-        final Point currentPoint = new Point(directions, 0);
-        final Point nextPoint = new Point(directions, 0);
-        assertThatThrownBy(() -> new Line(List.of(currentPoint, nextPoint)))
+    void 오른쪽_방향_또는_왼쪽_방향을_가진_포인트들이_연속으로_등장할_경우_예외가_발생한다(final Direction... directions) {
+        final Point firstPoint = point(Direction.BOTTOM);
+        final Point lastPoint = point(Direction.BOTTOM);
+        final Point targetPoint = point(directions);
+        final Point nextPoint = point(directions);
+
+        assertThatThrownBy(() -> new Line(List.of(firstPoint, targetPoint, nextPoint, lastPoint)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("오른쪽 방향 또는 왼쪽 방향이 연속으로 있을 수 없습니다.");
+    }
+
+    private Point point(final Direction... directions) {
+        return new Point(new Directions(directions), 0);
     }
 
     @Test
