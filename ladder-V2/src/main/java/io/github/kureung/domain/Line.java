f26e893b49f339b6848ccb639a28bcb263d242dd
kureungkureung <terry9611@naver.com>
2022-12-10 15:11:38 +0900

    feat: 행의 유효성 검증

--- ladder-V2/src/main/java/io/github/kureung/domain/Line.java
+++ ladder-V2/src/main/java/io/github/kureung/domain/Line.java
@@ -7,6 +7,9 @@ import static io.github.kureung.domain.Direction.RIGHT;
 
 public record Line(List<Point> points) {
     public Line {
+        verifyFirstPointDirections(points);
+        verifyLastPointDirections(points);
+
         for (int index = 0; index < points.size() - 1; index++) {
             final Point currentPoint = points.get(index);
             final Point nextPoint = points.get(index + 1);
@@ -23,14 +26,29 @@ public record Line(List<Point> points) {
         return Arrays.stream(points).toList();
     }
 
+    private void verifyFirstPointDirections(final List<Point> points) {
+        final Point firstPoint = points.get(0);
+        if (firstPoint.isIncludeDirection(Direction.LEFT)) {
+            throw new IllegalArgumentException("행의 첫번째 요소에는 왼쪽 방향을 가진 지점이 올 수 없습니다.");
+        }
+    }
+
+    private void verifyLastPointDirections(final List<Point> points) {
+        final Point lastPoint = points.get(points.size() - 1);
+        if (lastPoint.isIncludeDirection(RIGHT)) {
+            throw new IllegalArgumentException("행의 마지막 요소에는 오른쪽 방향을 가진 지점이 올 수 없습니다.");
+        }
+
+    }
+
     private void verifySameDirectionsExcludeBottom(final Point currentPoint, final Point nextPoint) {
-        if (currentPoint.doesntHaveOnlyBottom() &&  currentPoint.isSameDirections(nextPoint)) {
+        if (currentPoint.doesntHaveOnlyBottom() && currentPoint.isSameDirections(nextPoint)) {
             throw new IllegalArgumentException("오른쪽 방향 또는 왼쪽 방향이 연속으로 있을 수 없습니다.");
         }
     }
 
     private void verifyRightComeAfterLeft(final Point currentPoint, final Point nextPoint) {
-        if (currentPoint.isIncludeDirection(RIGHT) &&  nextPoint.isNotIncludeDirection(Direction.LEFT)) {
+        if (currentPoint.isIncludeDirection(RIGHT) && nextPoint.isNotIncludeDirection(Direction.LEFT)) {
             throw new IllegalArgumentException("오른쪽 방향을 가진 지점 다음에는 왼쪽 방향을 가진 지점이 와야합니다.");
         }
     }
--- ladder-V2/src/test/java/io/github/kureung/domain/LineTest.java
+++ ladder-V2/src/test/java/io/github/kureung/domain/LineTest.java
@@ -15,37 +15,40 @@ import static org.junit.jupiter.params.provider.Arguments.of;
 class LineTest {
     private static Stream<Arguments> 오른쪽_방향_또는_왼쪽_방향을_가진_포인트들이_연속으로_등장할_경우_예외가_발생한다() {
         return Stream.of(
-                of(Direction.LEFT, Direction.BOTTOM),
-                of(Direction.RIGHT, Direction.BOTTOM)
+                of(List.of(Direction.LEFT, Direction.BOTTOM)),
+                of(List.of(Direction.RIGHT, Direction.BOTTOM))
         );
-    }
-
-    @MethodSource
-    @ParameterizedTest
-    void 오른쪽_방향_또는_왼쪽_방향을_가진_포인트들이_연속으로_등장할_경우_예외가_발생한다(final Direction... directions) {
-        final Point firstPoint = point(Direction.BOTTOM);
-        final Point lastPoint = point(Direction.BOTTOM);
-        final Point targetPoint = point(directions);
-        final Point nextPoint = point(directions);
-
-        assertThatThrownBy(() -> new Line(List.of(firstPoint, targetPoint, nextPoint, lastPoint)))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("오른쪽 방향 또는 왼쪽 방향이 연속으로 있을 수 없습니다.");
     }
 
     private Point point(final Direction... directions) {
         return new Point(new Directions(directions), 0);
     }
 
+    private Point point(final List<Direction> directions) {
+        return new Point(new Directions(directions), 0);
+    }
+
+    @MethodSource
+    @ParameterizedTest
+    void 오른쪽_방향_또는_왼쪽_방향을_가진_포인트들이_연속으로_등장할_경우_예외가_발생한다(final List<Direction> directions) {
+        final Point firstPoint = point(Direction.BOTTOM);
+        final Point targetPoint = point(directions);
+        final Point nextPoint = point(directions);
+        final Point lastPoint = point(Direction.BOTTOM);
+
+        assertThatThrownBy(() -> new Line(firstPoint, targetPoint, nextPoint, lastPoint))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("오른쪽 방향 또는 왼쪽 방향이 연속으로 있을 수 없습니다.");
+    }
+
     @Test
     void 오른쪽_방향을_가진_지점_다음에는_왼쪽_방향을_가진_지점이_오지_않으면_예외가_발생한다() {
-        final Directions rightAndBottom = new Directions(Direction.RIGHT, Direction.BOTTOM);
-        final Directions onlyBottom = new Directions(Direction.BOTTOM);
+        final Point firstPoint = point(Direction.BOTTOM);
+        final Point targetPoint = point(Direction.RIGHT, Direction.BOTTOM);
+        final Point nextPoint = point(Direction.BOTTOM);
+        final Point lastPoint = point(Direction.BOTTOM);
 
-        final Point currentPoint = new Point(rightAndBottom, 0);
-        final Point nextPoint = new Point(onlyBottom, 0);
-
-        assertThatThrownBy(() -> new Line(currentPoint, nextPoint))
+        assertThatThrownBy(() -> new Line(firstPoint, targetPoint, nextPoint, lastPoint))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("오른쪽 방향을 가진 지점 다음에는 왼쪽 방향을 가진 지점이 와야합니다.");
     }
@@ -60,5 +63,25 @@ class LineTest {
 
         assertThatCode(() -> new Line(currentPoint, nextPoint))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    void 행의_첫번째_요소에_왼쪽_방향을_가진_지점이_올_경우_예외가_발생한다() {
+        final Directions leftAndBottom = new Directions(Direction.LEFT, Direction.BOTTOM);
+        final Point firstPoint = new Point(leftAndBottom, 0);
+
+        assertThatThrownBy(() -> new Line(firstPoint))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("행의 첫번째 요소에는 왼쪽 방향을 가진 지점이 올 수 없습니다.");
+    }
+
+    @Test
+    void 행의_마지막_요소에_오른쪽_방향을_가진_지점이_올_경우_예외가_발생한다() {
+        final Directions rightAndBottom = new Directions(Direction.RIGHT, Direction.BOTTOM);
+        final Point lastPoint = new Point(rightAndBottom, 0);
+
+        assertThatThrownBy(() -> new Line(lastPoint))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("행의 마지막 요소에는 오른쪽 방향을 가진 지점이 올 수 없습니다.");
     }
 }
