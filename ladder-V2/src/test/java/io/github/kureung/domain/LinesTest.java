7ac6184fa57f550e685bd0a217b0aa6c7eec49dc
kureungkureung <terry9611@naver.com>
2022-12-11 21:04:26 +0900

    feat(Lines): 행들의 유효성 검증

--- ladder-V2/src/test/java/io/github/kureung/domain/LinesTest.java
+++ ladder-V2/src/test/java/io/github/kureung/domain/LinesTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LinesTest {
     @Test
@@ -16,5 +17,14 @@ class LinesTest {
         final Lines lines = new Lines(List.of(firstLine, secondLine));
 
         assertThat(lines.movedUser(user)).isZero();
+    }
+
+    @Test
+    void 행들의_각각의_행의_요소의_개수가_일치하지_않으면_예외가_발생한다() {
+        final Line firstLine = new Line(Direction.RIGHT_AND_BOTTOM, Direction.LEFT_AND_BOTTOM);
+        final Line secondLine = new Line(Direction.RIGHT_AND_BOTTOM, Direction.LEFT_AND_BOTTOM, Direction.BOTTOM);
+        assertThatThrownBy(() -> new Lines(List.of(firstLine, secondLine)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("각각의 행의 요소 개수가 일치하지 않습니다.");
     }
 }
