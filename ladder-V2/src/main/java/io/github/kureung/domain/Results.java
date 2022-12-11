01a5692676cbe8213491715ebdb1e1e614925b4a
kureungkureung <terry9611@naver.com>
2022-12-11 21:11:47 +0900

    feat(Results): 당첨 결과 조회 인덱스의 유효성 검증

--- ladder-V2/src/main/java/io/github/kureung/domain/Results.java
+++ ladder-V2/src/main/java/io/github/kureung/domain/Results.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 public record Results(List<String> values) {
     public String result(final int index) {
+        if (index < 0 || values.size() <= index) {
+            throw new IllegalArgumentException("인덱스가 올바르지 않습니다.");
+        }
         return values.get(index);
     }
 }
