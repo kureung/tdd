0e78b299480787b903b89894e7d2de9dfe8442e3
kureungkureung <terry9611@naver.com>
2022-12-22 23:34:24 +0900

    feat(Rectangle): 직사각형 가로 길이, 세로 길이 유효성 검증

--- rectangular-room-area/src/main/java/io/github/kureung/domain/Rectangle.java
+++ rectangular-room-area/src/main/java/io/github/kureung/domain/Rectangle.java
@@ -1,6 +1,12 @@
 package io.github.kureung.domain;
 
 public record Rectangle(int widthInFeet, int heightInFeet) {
+    public Rectangle {
+        if (widthInFeet <= 0 || heightInFeet <= 0) {
+            throw new IllegalArgumentException("가로 길이와 세로 길이는 양수이어야 합니다.");
+        }
+    }
+
     public int areaInFeet() {
         return widthInFeet * heightInFeet;
     }
--- rectangular-room-area/src/test/java/io/github/kureung/domain/RectangleTest.java
+++ rectangular-room-area/src/test/java/io/github/kureung/domain/RectangleTest.java
@@ -1,13 +1,27 @@
 package io.github.kureung.domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class RectangleTest {
     @Test
     void 직사각형의_면적을_피트로_구할_수_있다() {
         final Rectangle rectangle = new Rectangle(15, 20);
         assertThat(rectangle.areaInFeet()).isEqualTo(300);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "0, 1",
+            "1, -1",
+    })
+    void 직사각형의_가로길이_또는_세로길이가_양수가_아닐_경우_예외가_발생한다(final int widthInFeet, final int heightInFeet) {
+        assertThatThrownBy(() -> new Rectangle(widthInFeet, heightInFeet))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("가로 길이와 세로 길이는 양수이어야 합니다.");
     }
 }
