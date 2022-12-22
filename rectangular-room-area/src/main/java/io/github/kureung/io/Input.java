ae8a1bd1b8d9fd251308b8d6f780217bd267e40a
kureungkureung <terry9611@naver.com>
2022-12-22 23:49:47 +0900

    feat(Input): 세로 길이 입력 받기

---
+++ rectangular-room-area/src/main/java/io/github/kureung/io/Input.java
@@ -0,0 +1,14 @@
+package io.github.kureung.io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public record Input() {
+    private static BufferedReader BUFFERED_READER = new BufferedReader(new InputStreamReader(System.in));
+
+    public int length() throws IOException {
+        System.out.print("What is hte length of hte room in feet?");
+        return Integer.parseInt(BUFFERED_READER.readLine());
+    }
+}
