3fef430431c97b1a92d64d9220739a3b3a94618e
kureungkureung <terry9611@naver.com>
2022-12-23 19:04:15 +0900

    feat(Pizzas): 피자들의 생성자 구하기

--- pizza-party/src/main/java/io/github/kurueng/domain/Pizzas.java
+++ pizza-party/src/main/java/io/github/kurueng/domain/Pizzas.java
@@ -1,8 +1,23 @@
 package io.github.kurueng.domain;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.IntStream;
 
-public record Pizzas(List<Pizza> values) {
+public class Pizzas {
+
+    private final List<Pizza> values;
+    Pizzas(final List<Pizza> values) {
+        this.values = values;
+    }
+
+    public static Pizzas initBy(final int numberOfPiecePerPizza, final int numberOfPizza) {
+        final List<Pizza> pizzas = IntStream.range(0, numberOfPizza)
+                .mapToObj(pizza -> new Pizza(numberOfPiecePerPizza))
+                .toList();
+        return new Pizzas(pizzas);
+    }
+
     public int piecesPerPerson(final People eaters) {
         final int pieces = pieces();
         return pieces / eaters.countPeople();
@@ -20,5 +35,18 @@ public record Pizzas(List<Pizza> values) {
 
     public int numberOfPiecesPerPizza() {
         return values.get(0).pieces();
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Pizzas pizzas = (Pizzas) o;
+        return Objects.equals(values, pizzas.values);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(values);
     }
 }
