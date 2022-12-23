481846b655af97e8dbacaf1b8bf1290fc087f4fd
kureungkureung <terry9611@naver.com>
2022-12-23 19:14:34 +0900

    feat(PizzaGame): 피자 게임 기능 추가

--- pizza-party/src/main/java/io/github/kurueng/application/PizzaGame.java
+++ pizza-party/src/main/java/io/github/kurueng/application/PizzaGame.java
@@ -4,11 +4,23 @@ import io.github.kurueng.domain.People;
 import io.github.kurueng.domain.Pizzas;
 
 public record PizzaGame(Pizzas pizzas, People people) {
+    public PizzaGame(final int numberOfPiecePerPizza, final int numberOfPizza, final int peopleCount) {
+        this(Pizzas.initBy(numberOfPiecePerPizza, numberOfPizza), new People(peopleCount));
+    }
+
     public int piecesPerPerson() {
         return pizzas.piecesPerPerson(people);
     }
 
     public int leftoversPieces() {
         return pizzas.leftoversPieces(people);
+    }
+
+    public int numberOfPizza() {
+        return pizzas.numberOfPizza();
+    }
+
+    public int numberOfPiecesPerPizza() {
+        return pizzas.numberOfPiecesPerPizza();
     }
 }
