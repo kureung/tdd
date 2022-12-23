3b5cc19ae210d8a09d526edf9ff639e97ebff4c9
kureungkureung <terry9611@naver.com>
2022-12-23 18:27:06 +0900

    feat(Pizzas): 피자를 나눈 후 남은 조각 구하기

--- pizza-party/src/main/java/io/github/kurueng/domain/Pizzas.java
+++ pizza-party/src/main/java/io/github/kurueng/domain/Pizzas.java
@@ -4,9 +4,17 @@ import java.util.List;
 
 public record Pizzas(List<Pizza> values) {
     public int piecesPerPerson(final People eaters) {
-        final int pieces = values.stream()
+        final int pieces = pieces();
+        return pieces / eaters.countPeople();
+    }
+
+    public int leftoversPeieces(final People eaters) {
+        return pieces() % eaters.countPeople();
+    }
+
+    private int pieces() {
+        return values.stream()
                 .mapToInt(Pizza::pieces)
                 .sum();
-        return pieces / eaters.countPeople();
     }
 }
--- pizza-party/src/test/java/io/github/kurueng/domain/PizzasTest.java
+++ pizza-party/src/test/java/io/github/kurueng/domain/PizzasTest.java
@@ -13,4 +13,11 @@ class PizzasTest {
         final Pizzas pizzas = new Pizzas(List.of(new Pizza(8)));
         assertThat(pizzas.piecesPerPerson(eaters)).isEqualTo(4);
     }
+
+    @Test
+    void 피자를_나눈_후_남은_조각을_구할_수_있다() {
+        final People eaters = new People(3);
+        final Pizzas pizzas = new Pizzas(List.of(new Pizza(4)));
+        assertThat(pizzas.leftoversPeieces(eaters)).isEqualTo(1);
+    }
 }
