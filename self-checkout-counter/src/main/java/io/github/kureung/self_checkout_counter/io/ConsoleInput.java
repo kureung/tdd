3ffa1167017d69153aabf136785791773faf9f61
kureungkureung <terry9611@naver.com>
2023-01-04 22:14:15 +0900

    refactor(ConsoleInput): 입력기 이름 변경

--- self-checkout-counter/src/main/java/io/github/kureung/self_checkout_counter/io/ConsoleInput.java
+++ self-checkout-counter/src/main/java/io/github/kureung/self_checkout_counter/io/ConsoleInput.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Input {
+public class ConsoleInput {
     private static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
 
     public int price(final int itemNumber) throws IOException {
